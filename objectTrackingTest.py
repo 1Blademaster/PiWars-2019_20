6dff9fd9061f9a0724e18691fa7c989da83e22c6
1Blademaster <blademaster1101@gmail.com>
2019-10-29 01:20:30 +0000

    shows all contours and makes largest green

--- objectTrackingTest.py
+++ objectTrackingTest.py
@@ -24,18 +24,23 @@ while True:
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     center = None
 
-    # only proceed if at least one contour was found
     if len(cnts) > 0:
-        # find the largest contour in the mask
-        c = max(cnts, key=cv2.contourArea)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])) #This sets the variable center to the location of the center in a tuple of (x, y)
+        maxCNT = max(cnts, key=cv2.contourArea)
+        maxM = cv2.moments(maxCNT)
+        maxCenter = (int(maxM["m10"] / maxM["m00"]), int(maxM["m01"] / maxM["m00"])) #This sets the variable center to the location of the center in a tuple of (x, y)
 
-        cv2.circle(frame, center, 5, (0, 0, 255), -1) #Displays the center location of the object
+        cv2.circle(frame, maxCenter, 5, (0, 255, 0), -1)
 
-        if center[0] < 300:
+        for cnt in cnts:
+            if np.all(cnt != maxCNT):
+                M = cv2.moments(cnt)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+                cv2.circle(frame, center, 5, (0, 0, 255), -1) #Displays the center location of the object
+
+        if maxCenter[0] < 300:
             print('Object is on left')
-        if center[0] > 600:
+        if maxCenter[0] > 600:
             print('Object is on right')
 
     cv2.imshow("Frame", frame)
