9a4b30fdcc137c8d6573dfe500a6577d2c6f822f
1blademaster <blademaster1101@gmail.com>
2019-11-07 13:46:27 +0000

    worked on js on remote

--- onlineRemote/remote.py
+++ onlineRemote/remote.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
+movement = {
+    'direction': '',
+}
+
 @app.route('/slider')
 def slider():
     return render_template('slider.html')
@@ -14,6 +18,26 @@ def fetchSliderData():
 @app.route('/keyPress')
 def keyPress():
     return render_template('keyPress.html')
+
+@app.route('/fetchKeyPressData', methods=['POST'])
+def fetchKeyPressData():
+    print(request.json)
+    if request.json == 'left':
+        if movement['direction'] == 'right':
+            movement['direction'] = 'stopped'
+    elif request.json == 'right':
+        if movement['direction'] == 'left':
+            movement['direction'] = 'stopped'
+    elif request.json == 'up':
+        if movement['direction'] == 'down':
+            movement['direction'] = 'stopped'
+    elif request.json == 'down':
+        if movement['direction'] == 'up':
+            movement['direction'] = 'stopped'
+    else:
+        movement['direction'] = request.json
+    
+    return movement['direction']
 
 if __name__ == '__main__':
     app.run(debug=True)
