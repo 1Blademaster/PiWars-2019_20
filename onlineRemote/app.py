ce0052e39ae6c43bb2dd622c9a6881f0d1e76f13
1Blademaster <blademaster1101@gmail.com>
2019-11-10 21:11:38 +0000

    Made imports work

--- onlineRemote/app.py
+++ onlineRemote/app.py
@@ -1,8 +1,14 @@
+#https://www.techcoil.com/blog/how-to-use-flask-apscheduler-in-your-python-3-flask-application-to-run-multiple-tasks-in-parallel-from-a-single-http-request/
+
 from flask import Flask, render_template, url_for, request, jsonify
-from robot import Robot
+import os
+import sys
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from motorFunctions.robot import Robot
 
 app = Flask(__name__)
-robot = Robot()
+#robot = Robot()
 movement = {
     'direction': None,
     'speed': None,
@@ -18,7 +24,7 @@ def fetchSliderData():
     movement['speed'] = request.json
     return jsonify({'response': 'done'})
 
-@app.route('/keyPress')
+@app.route('/')
 def keyPress():
     return render_template('keyPress.html')
 
@@ -28,31 +34,31 @@ def fetchKeyPressData():
     if request.json == 'left':
         if movement['direction'] == 'right':
             movement['direction'] = 'stopped'
-            robot.stop()
+            #robot.stop()
         else:
             movement['direction'] = 'left'
-            robot.turnLeft(9999999)
+            #robot.turnLeft(9999999)
     elif request.json == 'right':
         if movement['direction'] == 'left':
             movement['direction'] = 'stopped'
-            robot.stop()
+            #robot.stop()
         else:
             movement['direction'] = 'right'
-            robot.turnRight(9999999)
+            #robot.turnRight(9999999)
     elif request.json == 'up':
         if movement['direction'] == 'down':
             movement['direction'] = 'stopped'
-            robot.stop()
+            #robot.stop()
         else:
             movement['direction'] = 'up'
-            robot.forward(9999999)
+            #robot.forward(9999999)
     elif request.json == 'down':
         if movement['direction'] == 'up':
             movement['direction'] = 'stopped'
-            robot.stop()
+            #robot.stop()
         else:
             movement['direction'] = 'down'
-            robot.backward(9999999)
+            #robot.backward(9999999)
     else:
         print('Unknown input')
 
